1616b75f162c7e60ed4555b5c715da198987171e
Joris <joris.conrad@icloud.com>
2025-11-30 18:06:55 +0100

    Initial Commit

---
+++ Govee Mac/Govee_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Govee_MacApp.swift
+//  Govee Mac
+//
+//  Created by Joris Conrad on 30.11.25.
+//
+
+import SwiftUI
+
+@main
+struct Govee_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
